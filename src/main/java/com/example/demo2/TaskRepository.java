6179ab8be960ac4b9af5327462880a1b1f5b5043
Eric <ericdirla1000@gmail.com>
2024-06-04 15:22:46 +0200

    fix comments

--- src/main/java/com/example/demo2/TaskRepository.java
+++ src/main/java/com/example/demo2/TaskRepository.java
@@ -4,9 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.example.demo2.Tasks;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-// CRUD refers Create, Read, Update, Delete
-
 public interface TaskRepository extends CrudRepository<Tasks, Integer> {
 
 }
--- src/main/java/com/example/demo2/UserRepository.java
+++ src/main/java/com/example/demo2/UserRepository.java
@@ -6,9 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.example.demo2.Users;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-// CRUD refers Create, Read, Update, Delete
-
 public interface UserRepository extends CrudRepository<Users, Integer> {
     Optional<Users> findByUsername(String username);
 
