98e67f9cf4d0b68aca72a9d092fe6e4e92f934a8
Eric <ericdirla1000@gmail.com>
2024-06-20 11:42:38 +0200

    Add Comment

--- src/test/java/com/example/demo2/ArchitectureTests.java
+++ src/test/java/com/example/demo2/ArchitectureTests.java
@@ -52,7 +52,8 @@ public class ArchitectureTests {
             .should().beFreeOfCycles()
             .check(importedClasses);
     }
-
+    
+    // Dependency Testing für Controller, Repositories und Entitäten
     @Test
     public void controllersShouldOnlyUseOtherLayers() {
         ArchRule rule = classes().that().areAnnotatedWith(Controller.class)
